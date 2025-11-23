bf62409d13da0a72d31f8aebbf3de81fed49b325
Just Playin <ensminger@yahoo.com>
2025-11-22 21:06:19 -0500

    Add script to install Docker and Docker Desktop
    Install Docker and Docker Desktop on the system.

--- desktop.sh
+++ desktop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+clear
+sudo apt install -y curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+bash get-docker.sh
 sudo apt install -y wget
 wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
 sudo dpkg -i docker-desktop-amd64.deb
