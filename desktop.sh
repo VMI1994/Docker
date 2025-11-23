135c1a1ff7da287cb6550c45ef2118bcef185fee
Just Playin <ensminger@yahoo.com>
2025-11-23 09:17:16 -0500

    Update Docker installation process in desktop.sh
    Replaced Docker installation method with a script and added download for Docker Desktop.

--- desktop.sh
+++ desktop.sh
@@ -4,22 +4,9 @@ clear
 # Deps
 sudo apt install -y curl wget ca-certificates
 
-# Add Docker's official GPG key:
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-Components: stable
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-sudo apt update
-
-
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+bash get-docker.sh
 wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
 sudo dpkg -i docker-desktop-amd64.deb
 sudo apt --fix-broken install -y
