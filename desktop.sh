7be28d5bb06d7d085cbc1247c1a51f66f868626e
Just Playin <ensminger@yahoo.com>
2025-11-23 08:46:59 -0500

    Remove Docker installation steps from desktop.sh
    Removed installation steps for Portainer, Watchtower, and Open-WebUI from the script.

--- desktop.sh
+++ desktop.sh
@@ -34,37 +34,8 @@ read pause
 # Enable and start Docker
 sudo usermod -aG docker $USER
 
-# Install portainer and watchtower
-clear
-echo "Install portainer and watchtower"
-sleep 2
-sudo docker volume create portainer_data
-sudo docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-clear
-sudo docker run  -d -v /var/run/docker.sock:/var/run/docker.sock --name watchtower nickfedor/watchtower
-
-# Option to install open-webui and ollama
-clear
-echo "Do you want to install Artificial Intelligence (y/N)"
-read option
-if [ $option == "y" ]
-then
-    sudo docker run -d -p 3000:8080 -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
-    sudo docker exec -it open-webui ollama pull gemma3:1b
-    clear && "OpenWebUI is available at http://127.0.0.1:3000"
-fi
-
-# Check Containers
-clear
-echo ""
-echo ""
-echo "We will now check running docker containers"
-sleep 2
-sudo docker ps
-echo ""
-echo ""
-
 # Check install
+clear 
 which docker > /dev/null
 if [ $? == 0 ]
 then
